5d72b236fa0b7ccb59363619e9cf23c5142e7934
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-05-20 23:58:21 +0430

    add about and hero sections

---
+++ app/home/static/js/tabs.js
@@ -0,0 +1,15 @@
+function showTab(e, tabName) {
+    var i, tabContent, tabBtn;
+    tabContent = document.getElementsByClassName("about-tabs__content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabBtn = document.getElementsByClassName("nav-btn");
+    for (i = 0; i < tabBtn.length; i++) {
+        tabBtn[i].className = tabBtn[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    e.currentTarget.className += " active";
+}
+
+showTab();
--- app/static/js/slider.js
+++ app/static/js/slider.js
@@ -1,3 +1,4 @@
 function toggleSlider() {
     document.getElementById("slide").classList.toggle('toggling');
+    document.getElementById("overlay").classList.toggle('open');
 }
