e042abcc547c46a3b4048df688b85d620ce813f0
iam-benyamin <benyaminmahmoudyan@gmail.com>
2022-09-10 18:34:43 +0430

    fixbug/change-contact-form-int-to-char

--- app/home/models/contact.py
+++ app/home/models/contact.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Contact(models.Model):
-    number = models.IntegerField()
+    number = models.CharField(max_length=20)
     email = models.EmailField(max_length=254)
     date = models.DateTimeField()
