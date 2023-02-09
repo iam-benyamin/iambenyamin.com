2948d42fb05a0d2d242925519f1393f020d8415e
iam-benyamin <benyaminmahmoudyan@gmail.com>
2023-02-09 19:48:59 +0330

    ci/github-action-test

--- app/config/settings/local.py
+++
@@ -1,2 +0,0 @@
-
-melipayamak_token = 'https://console.melipayamak.com/api/send/simple/220260e87e25415a84e4532c7d02fea7'
--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -13,7 +13,6 @@ ALLOWED_HOSTS = [
     'mail.iambenyamin.com',
 ]
 
-
 # database config for postgreql
 DATABASES = {
     'default': {
@@ -26,9 +25,7 @@ DATABASES = {
     }
 }
 
-
 TIME_ZONE = 'Asia/Tehran'
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
