86e4c3cf7fd03ec4de40c97b5d525c684df22522
iam-benyamin <benyaminmahmoudyan@gmail.com>
2023-08-16 22:57:09 +0330

    feat: comment my cv sction in url and template

--- app/portfolio/urls.py
+++ app/portfolio/urls.py
@@ -12,5 +12,5 @@ app_name = 'portfolio'
 urlpatterns = [
     path('list/', PortfolioListView.as_view(), name='list-view'),
     path('detail/<slug:slug>/', PortfolioDetailView.as_view(), name='detail-view'),
-    path('<slug:slug>/', MyCVDetailView.as_view(), name='cv-view'),
+    # path('<slug:slug>/', MyCVDetailView.as_view(), name='cv-view'),
 ]
